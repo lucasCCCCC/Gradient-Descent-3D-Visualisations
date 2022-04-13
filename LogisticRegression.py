2002eb6a435b7dcf0f1718a6b6db704a6c7f4453
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-04-14 00:30:44 +0100

    Trained the Logistic Regression model over some test data

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class LogisticRegression:
@@ -27,3 +28,16 @@ class LogisticRegression:
         self.w0 = self.w0 - self.learningRate * w0_pd
         self.w1 = self.w1 - self.learningRate * w1_pd
 
+
+test_data = np.array([[1, 0], [2, 0], [3, 0], [4, 0], [5, 1], [6, 1], [7, 1], [8, 1]])
+
+x_train_data = test_data[:, :1]
+y_train_data = test_data[:, 1:]
+
+model = LogisticRegression(20, 10, 0.5)
+
+for i in range(30000):
+    model.gradientDescent(x_train_data, y_train_data)
+
+
+print("Final Model: ", "y = 1/ 1 + exp(-(", model.getWeights()[0], "x + ", model.getWeights()[1], "))")
